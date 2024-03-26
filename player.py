7303d45d99d9594ff5afaf34ea26a6a5de428f42
Roy Mutsoli Msoli <143260903+MsoliRoy@users.noreply.github.com>
2024-03-26 20:33:45 +0300

    Update player.py

--- player.py
+++ player.py
@@ -2,28 +2,51 @@ import random
 from enum import Enum
 
 class PlayerType(Enum):
+    """Enumeration representing player types."""
     HUMAN = 0
     RANDOM_AGENT = 1
 
 class Player:
     """Class representing a player in the TicTacToe game."""
-
+    
     def __init__(self, letter, playerType=PlayerType.HUMAN):
-        """Initialize the player."""
+        """
+        Initialize the player.
+
+        Parameters:
+        - letter (str): The letter representing the player on the game board.
+        - playerType (PlayerType): The type of player (default: PlayerType.HUMAN).
+        """
         self.letter = letter
         self.playerType = playerType
 
-    def makeMove(self, board):
-        """Make a move on the board."""
+    def make_move(self, board):
+        """
+        Make a move on the board.
+
+        Parameters:
+        - board (list): The current state of the game board.
+
+        Returns:
+        - int or tuple: The move made by the player.
+        """
         if self.playerType == PlayerType.HUMAN:
-            return self.requestMove()
+            return self.request_move()
         elif self.playerType == PlayerType.RANDOM_AGENT:
-            return self.makeRandomMove(board)
+            return self.make_random_move(board)
         else:
             return None
 
-    def makeRandomMove(self, board):
-        """Make a random move on the board."""
+    def make_random_move(self, board):
+        """
+        Make a random move on the board.
+
+        Parameters:
+        - board (list): The current state of the game board.
+
+        Returns:
+        - tuple: The random move made by the player.
+        """
         available_moves = []
         for i in range(3):
             for j in range(3):
@@ -31,8 +54,13 @@ class Player:
                     available_moves.append((i+1, j+1))
         return random.choice(available_moves)
 
-    def requestMove(self):
-        """Request a move from the human player."""
+    def request_move(self):
+        """
+        Request a move from the human player.
+
+        Returns:
+        - int: The move entered by the human player.
+        """
         while True:
             try:
                 move = int(input("Enter move (0-8): "))
@@ -41,3 +69,5 @@ class Player:
                 return move
             except ValueError:
                 print("Invalid move. Please enter a number between 0 and 8.")
+
+# Add TicTacToe class and main game loop here
