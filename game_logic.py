949a2f05168f3093c854d44835cc85c85549062b
Roy Mutsoli Msoli <143260903+MsoliRoy@users.noreply.github.com>
2024-03-26 20:36:57 +0300

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -1,8 +1,19 @@
 class GameLogic:
-    def __init__(self, board_size=3, win_condition=3):
-        self.game = TicTacToe(board_size, win_condition)
+    def __init__(self, game):
+        self.game = game
 
-    def makeMove(self, location, mark):
-        return self.game.makeMove(location, mark)
+    def make_move(self, location, mark):
+        """Make a move on the game."""
+        return self.game.make_move(location, mark)
 
     # Add other game logic methods...
+
+# Example usage:
+# Instantiate the TicTacToe game
+game = TicTacToe()
+
+# Initialize game logic with the TicTacToe instance
+game_logic = GameLogic(game)
+
+# Use game logic to make moves
+game_logic.make_move((0, 0), 'X')
