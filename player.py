b2586e0b6d3a99c861f7af9b6e76daed619a090c
Roy Mutsoli Msoli <143260903+MsoliRoy@users.noreply.github.com>
2024-03-26 16:58:20 +0300

    Update player.py

--- player.py
+++ player.py
@@ -6,17 +6,38 @@ class PlayerType(Enum):
     RANDOM_AGENT = 1
 
 class Player:
+    """Class representing a player in the TicTacToe game."""
+
     def __init__(self, letter, playerType=PlayerType.HUMAN):
+        """Initialize the player."""
         self.letter = letter
         self.playerType = playerType
 
-    # Other methods of Player class
-    # ...
+    def makeMove(self, board):
+        """Make a move on the board."""
+        if self.playerType == PlayerType.HUMAN:
+            return self.requestMove()
+        elif self.playerType == PlayerType.RANDOM_AGENT:
+            return self.makeRandomMove(board)
+        else:
+            return None
 
     def makeRandomMove(self, board):
-        # Random move logic
-        # ...
+        """Make a random move on the board."""
+        available_moves = []
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == ' ':
+                    available_moves.append((i+1, j+1))
+        return random.choice(available_moves)
 
     def requestMove(self):
-        # Human player move logic
-        # ...
+        """Request a move from the human player."""
+        while True:
+            try:
+                move = int(input("Enter move (0-8): "))
+                if move < 0 or move > 8:
+                    raise ValueError
+                return move
+            except ValueError:
+                print("Invalid move. Please enter a number between 0 and 8.")
